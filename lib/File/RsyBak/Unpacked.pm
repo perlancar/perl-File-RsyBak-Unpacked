835b10dfe7dd3187314137138f59a76370fd8541
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-21 20:12:54 +0700

    Initial commit

---
+++ lib/File/RsyBak/Unpacked.pm
@@ -0,0 +1,20 @@
+package File::RsyBak::Unpacked;
+
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Backup files/directories with histories, using rsync (unpacked version)
+
+=head1 DESCRIPTION
+
+This distribution is for testing/development purposes. Users should just install
+the L<File::RsyBak> distribution because it has fewer dependencies.
+
+
+=head1 SEE ALSO
+
+L<File::RsyBak>
+
+L<rsybak>
